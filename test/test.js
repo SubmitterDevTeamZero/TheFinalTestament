0c3739c2e4db11a3d956dcd7461c2b67ea63ac77
Makan Azarshahy <oro1919@gmail.com>
2018-08-23 00:41:32 -0700

    Added tests for cleanVerse

--- test/test.js
+++ test/test.js
@@ -23,4 +23,56 @@ describe('### THE FINAL TESTAMENT ###', () => {
       expect(genQuery).to.throw(Error).with.property('message', 'BAD SURA AND/OR VERSE: Need to provide a number');
     });
   });
+
+  describe('___CLEAN VERSE___', () => {
+    const chapterNum = 63;
+    const verseNum = 6;
+    it('should be a function', () => {
+      expect(cleanVerse).to.be.a('function');
+    });
+
+    it('should return a formatted verse with the subtitle, verse, footnoes, and sura:verse', () => {
+      const stackedVerse = {
+        ZSUBTITLE: 'SUBTITLE',
+        ZENGLISH_VERSION: 'VERSE',
+        ZFOOTNOTE: 'FOOTNOTES',
+      };
+      const actual = cleanVerse(chapterNum, verseNum, stackedVerse);
+      expect(actual).to.be.a('string');
+      expect(actual).to.equal('SUBTITLE\n[63:6] VERSE\nFOOTNOTES');
+    });
+
+    it('shoult return a formatted verse with verse, footnotes, and sura:verse', () => {
+      const stackedVerse = {
+        ZSUBTITLE: null,
+        ZENGLISH_VERSION: 'VERSE',
+        ZFOOTNOTE: 'FOOTNOTES',
+      };
+      const actual = cleanVerse(chapterNum, verseNum, stackedVerse);
+      expect(actual).to.be.a('string');
+      expect(actual).to.equal('[63:6] VERSE\nFOOTNOTES');
+    });
+
+    it('shoult return a formatted verse with subtitle, verse, and sura:verse', () => {
+      const stackedVerse = {
+        ZSUBTITLE: 'SUBTITLE',
+        ZENGLISH_VERSION: 'VERSE',
+        ZFOOTNOTE: null,
+      };
+      const actual = cleanVerse(chapterNum, verseNum, stackedVerse);
+      expect(actual).to.be.a('string');
+      expect(actual).to.equal('SUBTITLE\n[63:6] VERSE');
+    });
+
+    it('shoult return a formatted verse with verse and sura:verse', () => {
+      const stackedVerse = {
+        ZSUBTITLE: null,
+        ZENGLISH_VERSION: 'VERSE',
+        ZFOOTNOTE: null,
+      };
+      const actual = cleanVerse(chapterNum, verseNum, stackedVerse);
+      expect(actual).to.be.a('string');
+      expect(actual).to.equal('[63:6] VERSE');
+    });
+  });
 });
